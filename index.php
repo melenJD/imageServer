a6b7a7dfacb867969dee2ca3222b4160b5ac00da
melenJD <el.todos17@gmail.com>
2020-03-29 00:09:00 +0900

    upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  $connect = mysqli_connect('localhost', 'admin_image', '!Rkawk135', 'admin_image-melen') or die ('connect fail');
+  $query = "SELECT * FROM image_user";
+  $result = $connect->query($query);
+  $total = @mysqli_num_rows($result);
+
+  session_start();
+
+  if(!isset($_SESSION['userid'])) {
+    header('Location: ./login.html');
+  }
+?>
--- php/login.php
+++ php/login.php
@@ -1,7 +1,27 @@
 <?php
   session_start();
-  if(!isset($_SESSION['userid']))
-  {
-    header('Location: ../index.html');
+
+  $connect = mysqli_connect('localhost', 'admin_image', '!Rkawk135', 'admin_image-melen');
+
+  $id=$_POST['userid'];
+  $pw=$_POST['userpwd'];
+
+  $query = "SELECT * FROM users WHERE userid='$id'";
+  $result = $connect->query($query);
+
+  if(mysqli_num_rows($result)==1) {
+    $row=mysqli_fetch_assoc($result);
+    if($row['userpwd']==$pw){
+      $_SESSION['userid']=$id;
+      if(isset($_SESSION['userid'])){
+        header('Location: main.php');
+      }else{
+        header("Refresh: 0; URL='../login.html'");
+        echo '<script>alert("비밀번호가 틀렸습니다");</script>';
+      }
+    }
+  }else{
+    header("Refresh: 0; URL='../login.html'");
+    echo '<script>alert("아이디가 없습니다");</script>';
   }
 ?>
